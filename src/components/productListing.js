841b28e3c04c133ff0ef5333c4e2d898cddc848d
Lameck1 <lameck72@gmail.com>
2022-12-06 12:57:21 +0300

    Add search funtionality

--- src/components/productListing.js
+++ src/components/productListing.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getProducts } from '../redux/products/products';
 import ProductComponents from './productComponents';
@@ -7,15 +7,39 @@ const ProductListing = () => {
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
 
+  useEffect(() => {
+    const results = products.filter(
+      (product) => product.title.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setSearchResults(results);
+  }, [products, searchTerm]);
+
   return (
-    <div className="ui centered grid container">
-      <ProductComponents
-        products={products}
+    <div className="main">
+      <input
+        type="text"
+        placeholder="Filter by product name..."
+        value={searchTerm}
+        onChange={handleChange}
       />
+      <div className="ui centered grid container">
+        <ProductComponents
+          products={
+            searchTerm.length > 0 ? searchResults : products
+          }
+        />
+      </div>
     </div>
   );
 };
